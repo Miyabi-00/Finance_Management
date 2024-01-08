6e405e157f04f094c514f4ee3c5b1f410dff665d
Miyabi-00 <pokezacattack@gmail.com>
2024-01-08 21:16:31 +0900

    add csv reader

--- Finance_Management.py
+++ Finance_Management.py
@@ -1,1 +1,9 @@
-#coding now
+
+import csv
+
+file = f"Rakuten_all.csv"
+
+with open(file, mode="r") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    for row in csv_reader:
+        print(row)
